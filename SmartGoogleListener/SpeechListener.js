585c1be128ff619aa95109c966f8d81f77837826
Sagar Patel <sagarpatel1313@gmail.com>
2018-09-12 22:50:37 -0400

    Addressed formatting comments

--- SmartGoogleListener/SpeechListener.js
+++ SmartGoogleListener/SpeechListener.js
@@ -3,7 +3,7 @@
 	var action;
 	var commands = ["search", "go to"];
 
-	if (!('webkitSpeechRecognition' in window)) {
+	if (!("webkitSpeechRecognition" in window)) {
 		console.log("Updated browser to support WebKit Speech Recognition API");
 	}
 	else {
@@ -12,7 +12,7 @@
 		recognition.continuous = true;
 		recognition.interimResults = false;
 		var recognizing = false;
-		var textRecorded = ''
+		var textRecorded = "";
 
 		recognition.onstart = function(event) {
 			speechText = [];
@@ -26,13 +26,13 @@
 			var searchText = "";
 			
 			console.log("Event result")
-			var interim_transcript = '';
+			var interim_transcript = "";
 			for (var i = event.resultIndex; i < event.results.length; ++i) {
-			  if (event.results[i].isFinal) {
+				if (event.results[i].isFinal) {
 				textRecorded += event.results[i][0].transcript;
-			  } else {
+				} else {
 				interim_transcript += event.results[i][0].transcript;
-			  }
+				}
 			}
 			console.log(textRecorded);
 			speechText = textRecorded.split(" ");
@@ -43,9 +43,9 @@
 						searchText = speechText.slice(1, speechText.length).join("+");
 						speechText[0] = speechText.slice(1, speechText.length).join("");
 						
-				for(var i = 0; i < speechText.length; i++){
-					console.log(speechText[i]);
-				}
+						for(var i = 0; i < speechText.length; i++){
+							console.log(speechText[i]);
+						}
 						recognition.stop();
 						break;
 					}
@@ -54,7 +54,7 @@
 			}
 			console.log(searchText);
 			if(searchText!=""){
-				window.open('https://www.google.com/search?q='+searchText);
+				window.open("https://www.google.com/search?q="+searchText);
 			}
 			
 			recognition.stop();
@@ -80,3 +80,4 @@
 			console.log(recognizing);
 		}
 	}
+	
